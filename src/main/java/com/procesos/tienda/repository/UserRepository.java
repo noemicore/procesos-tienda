db2e1f1195ab32c8a2f9a3a66e87abeec0ec5866
noemicore <94762677+noemicore@users.noreply.github.com>
2023-09-28 21:43:22 -0500

    Revert "Develop"

--- src/main/java/com/procesos/tienda/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.procesos.tienda.repository;
-
-import com.procesos.tienda.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
